f6161e695a44dd34d69415d1907226938892773c
Gael Mayer <gaelmayer@yahoo.fr>
2022-08-10 11:50:42 +0200

    First class commit

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 "use strict";
 const filterButton = document.querySelector('#filter');
-const showFilter = document.querySelector('#show-country');
+const showContinents = document.querySelector('#show-country');
 const blockFilter = document.querySelector('#filter-block');
 const regionDiv = document.querySelector('#regionDiv');
 const sectionCountry = document.querySelector('#section-country');
@@ -13,12 +13,13 @@ const request = fetch(url)
     let clicked = 1;
     filterButton === null || filterButton === void 0 ? void 0 : filterButton.addEventListener('click', displayRegion);
     function displayRegion() {
-        if (filterButton && showFilter && regionDiv) {
-            showFilter.style.display = 'block';
+        if (filterButton && showContinents && regionDiv) {
+            // Affiche la liste des continents
+            showContinents.style.display = 'block';
             for (let i = 0; i < data.length; i++) {
                 myArray.push(data[i].region);
             }
-            // Supprime les doublons
+            // Supprime les pays en doublons
             myOArray = [...new Set(myArray)];
             if (clicked === 1) {
                 for (let i = 0; i < myOArray.length; i++) {
@@ -32,49 +33,47 @@ const request = fetch(url)
             else if (clicked === 2) {
                 clicked = 0;
             }
-            // Renvoie les pays des régions
+            // Renvoie les pays des continents
             let liRegion = document.querySelectorAll('.regionLi');
             liRegion.forEach(function (element) {
                 element.addEventListener('click', () => {
+                    sectionCountry === null || sectionCountry === void 0 ? void 0 : sectionCountry.append('');
                     for (let i = 0; i <= data.length; i++) {
-                        if (element.innerHTML === data[i].region) {
-                            let blockCountry = document.createElement('div');
-                            blockCountry.className = 'block-country';
-                            let img = document.createElement('img');
-                            img.src = data[i].flags.png;
-                            let div = document.createElement('div');
-                            div.className = 'info-block';
-                            let title = document.createElement('h2');
-                            title.innerText = data[i].name.common;
-                            let ul = document.createElement('ul');
-                            let liData = [data[i].population, data[i].region, data[i].capital[0]];
-                            const dataObject = {
-                                Population: data[i].population,
-                                Region: data[i].region,
-                                Capital: data[i].capital[0]
-                            };
-                            for (let index = 0; index < liData.length; index++) {
-                                let li = document.createElement('li');
-                                for (const key in dataObject) {
-                                    if (dataObject[key] === liData[index]) {
-                                        li.textContent = `${key} : ${liData[index]}`;
-                                    }
+                        let blockCountry = document.createElement('div');
+                        blockCountry.className = 'block-country';
+                        let img = document.createElement('img');
+                        img.src = data[i].flags.png;
+                        let div = document.createElement('div');
+                        div.className = 'info-block';
+                        let title = document.createElement('h2');
+                        title.innerText = data[i].name.common;
+                        let ul = document.createElement('ul');
+                        let liData = [data[i].population, data[i].region, data[i].capital[0]];
+                        const dataObject = {
+                            Population: data[i].population,
+                            Region: data[i].region,
+                            Capital: data[i].capital[0]
+                        };
+                        for (let index = 0; index < liData.length; index++) {
+                            let li = document.createElement('li');
+                            for (const key in dataObject) {
+                                if (dataObject[key] === liData[index]) {
+                                    li.textContent = `${key} : ${liData[index]}`;
                                 }
-                                ul.append(li);
                             }
-                            div.append(title, ul);
-                            blockCountry === null || blockCountry === void 0 ? void 0 : blockCountry.append(img, div);
-                            sectionCountry === null || sectionCountry === void 0 ? void 0 : sectionCountry.append(blockCountry);
+                            ul.append(li);
                         }
+                        div.append(title, ul);
+                        blockCountry === null || blockCountry === void 0 ? void 0 : blockCountry.append(img, div);
+                        sectionCountry === null || sectionCountry === void 0 ? void 0 : sectionCountry.append(blockCountry);
                     }
                 });
             });
         }
     }
     blockFilter === null || blockFilter === void 0 ? void 0 : blockFilter.addEventListener('mouseleave', () => {
-        if (showFilter) {
-            showFilter.style.display = 'none';
-        }
+        if (showContinents)
+            showContinents.style.display = 'none';
     });
 })
     .catch(err => {
--- src/script/app.ts
+++ src/script/app.ts
@@ -1,5 +1,5 @@
 const filterButton = document.querySelector<HTMLButtonElement>('#filter');
-const showFilter = document.querySelector<HTMLElement>('#show-country');
+const showContinents = document.querySelector<HTMLElement>('#show-country');
 const blockFilter = document.querySelector<HTMLElement>('#filter-block')
 const regionDiv = document.querySelector<HTMLDivElement>('#regionDiv');
 const sectionCountry = document.querySelector<HTMLElement>('#section-country')
@@ -12,20 +12,20 @@ const request = fetch(url)
     let myArray: string[] = [];
     let myOArray: string[] = [];
     let clicked: number = 1;
-    
-    filterButton?.addEventListener('click', displayRegion)
 
+    filterButton?.addEventListener('click', displayRegion)
+    
     function displayRegion() {
         
+        if (filterButton && showContinents && regionDiv) {  
 
-        if (filterButton && showFilter && regionDiv) {  
-
-            showFilter.style.display = 'block';
+            // Affiche la liste des continents
+            showContinents.style.display = 'block';
             
             for ( let i = 0; i < data.length; i++) {
                 myArray.push(data[i].region)
             }
-            // Supprime les doublons
+            // Supprime les pays en doublons
             myOArray = [... new Set(myArray)]
             
             if (clicked === 1) {
@@ -43,57 +43,59 @@ const request = fetch(url)
                 clicked = 0;
             }
 
-            // Renvoie les pays des régions
+            // Renvoie les pays des continents
             let liRegion = document.querySelectorAll('.regionLi')
             liRegion.forEach(function (element) {
                 element.addEventListener('click', () => {
 
-                   for (let i = 0; i <= data.length; i++) {
+                  
+                    sectionCountry?.append('')      
+                    
+                    for (let i = 0; i <= data.length; i++) {
+                        
+                        
+                        let blockCountry = document.createElement('div')
+                        blockCountry.className = 'block-country'                            
 
-                        if (element.innerHTML === data[i].region) {
+                        let img: HTMLImageElement = document.createElement('img')
+                        img.src = data[i].flags.png
 
-                            let blockCountry = document.createElement('div')
-                            blockCountry.className = 'block-country'
+                        let div = document.createElement('div')
+                        div.className = 'info-block'
 
-                            let img: HTMLImageElement = document.createElement('img')
-                            img.src = data[i].flags.png
+                        let title = document.createElement('h2')
+                        title.innerText = data[i].name.common
+                        let ul: HTMLUListElement = document.createElement('ul')
+                        let liData: string[] = [data[i].population, data[i].region, data[i].capital[0]]
+                        
+                        const dataObject: {[key: string]: string} =
+                        {
+                            Population : data[i].population,
+                            Region : data[i].region,
+                            Capital : data[i].capital[0]
+                        }
 
-                            let div = document.createElement('div')
-                            div.className = 'info-block'
-
-                            let title = document.createElement('h2')
-                            title.innerText = data[i].name.common
-                            let ul: HTMLUListElement = document.createElement('ul')
-                            let liData: string[] = [data[i].population, data[i].region, data[i].capital[0]]
+                        for (let index = 0; index < liData.length; index++) {
+                            let li: HTMLLIElement = document.createElement('li')
                             
-                            const dataObject: {[key: string]: any} =
-                            {
-                                Population : data[i].population,
-                                Region : data[i].region,
-                                Capital : data[i].capital[0]
-                            }
+                            for (const key in dataObject) {
+                                    
+                                if (dataObject[key] === liData[index]) {
+                                    
+                        
+                                    li.textContent = `${key} : ${liData[index]}`
 
-                            for (let index = 0; index < liData.length; index++) {
-                                let li: HTMLLIElement = document.createElement('li')
-                                
-                                for (const key in dataObject) {
-                                        
-                                    if (dataObject[key] === liData[index]) {
-                                     
+                                    
+                                }
+                            }                                
+                            ul.append(li)
                             
-                                        li.textContent = `${key} : ${liData[index]}`
-
-                                        
-                                    }
-                                }                                
-                                ul.append(li)
-                                
-                            }
-                                div.append(title, ul)
-                                blockCountry?.append(img, div)    
-                                sectionCountry?.append(blockCountry)                                                     
+                        }
+                        div.append(title, ul)
+                        blockCountry?.append(img, div)    
+                        sectionCountry?.append(blockCountry)                                                     
                             
-                        }                        
+                                          
                         
                     }
                 })
@@ -101,13 +103,12 @@ const request = fetch(url)
 
         }
     }
+
+    
     
     blockFilter?.addEventListener('mouseleave', () => {
-        if (showFilter) {
-            
-            showFilter.style.display = 'none'
-        }
-        
+        if (showContinents)     
+            showContinents.style.display = 'none'
     })
 
 })
