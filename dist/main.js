dc1255ca29b8c0b2236803050a3030e4de06d300
Gael Mayer <gaelmayer@yahoo.fr>
2022-08-18 09:51:48 +0200

    Delete data without capital

--- dist/main.js
+++ dist/main.js
@@ -10,9 +10,16 @@ const request = fetch(url)
 .then(response => response.json())
 .then(data => {
 
-    
     for (let i = 0; i < data.length; i++) {
-        
+
+        if (!data[i].hasOwnProperty('capital')) {
+            delete data[i]
+            i++
+        }        
+     
+
+
+
 
         let blockCountry = document.createElement('div')
         let ul = document.createElement('ul')
@@ -24,15 +31,15 @@ const request = fetch(url)
         img.src = data[i].flags.png
         title.innerText = data[i].name.common
         
+        
         let liData = [data[i].population, data[i].region, data[i].capital[0]]
-        //console.log(data[i].capital[0]);
+        
 
-        const dataObject = {
+        let dataObject = {
             Population : data[i].population,
             Region : data[i].region,
             Capital : data[i].capital[0]
         }
-        console.log(dataObject)
         
         for (let index = 0; index < liData.length; index++) {
             let li = document.createElement('li')
@@ -44,7 +51,6 @@ const request = fetch(url)
                 }
                 
             }   
-            console.log(li);                             
             ul.append(li)
             
         }
